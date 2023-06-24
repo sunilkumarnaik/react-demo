fddd44edbbc781bf0df46fa9e715ebd2580624e4
naiksunilkumar <inbox2nil@gmail.com>
2023-06-24 13:00:01 +0530

    Initial commit

---
+++ src/Body.js
@@ -0,0 +1,4 @@
+function body() {
+  return <h1>This is inside body</h1>;
+}
+export default body;
---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+    return <h4 style={{"color":"red"}}>Footer</h4>
+}
+
+export default Footer;
